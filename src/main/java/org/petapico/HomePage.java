f61da3b33d8fe2c1bd9112019f2cf37f1af284db
Tobias Kuhn <kuhntobias@gmail.com>
2019-01-04 13:52:36 +0100

    Use RepeatingView

--- src/main/java/org/petapico/HomePage.java
+++ src/main/java/org/petapico/HomePage.java
@@ -3,9 +3,7 @@ package org.petapico;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.markup.repeater.RepeatingView;
 
 /**
  * Homepage
@@ -23,15 +21,16 @@ public class HomePage extends WebPage {
 	 *            Page parameters
 	 */
     public HomePage(final PageParameters parameters) {
-        add(new DataView<String>("users", new ListDataProvider<String>(Utils.getUsers())) {
-
-			private static final long serialVersionUID = -7900012913964111340L;
-
-			@Override
-        	protected void populateItem(Item<String> item) {
-        		item.add(new Label("userid", item.getModelObject()));
-        	}
-
-        });
+//        add(new DataView<String>("users", new ListDataProvider<String>(Utils.getUsers())) {
+//			@Override
+//        	protected void populateItem(Item<String> item) {
+//        		item.add(new Label("userid", item.getModelObject()));
+//        	}
+//        });
+        RepeatingView userIdItems = new RepeatingView("userid");
+        for (String u : Utils.getUsers()) {
+        	userIdItems.add(new Label(userIdItems.newChildId(), u));
+        }
+        add(userIdItems);
     }
 }
