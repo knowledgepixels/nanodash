0008a9264548f43eb1fca14255d64e15b3be82b1
Tobias Kuhn <kuhntobias@gmail.com>
2019-01-04 13:46:09 +0100

    Check multiple APIs

--- src/main/java/org/petapico/Utils.java
+++ src/main/java/org/petapico/Utils.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -30,6 +32,11 @@ public class Utils {
 		httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
 	}
 
+	public static String[] apiInstances = new String[] {
+		"http://grlc.io/api/peta-pico/nanopub-api/",
+		"http://grlc.nanopubs.d2s.labs.vu.nl/api/local/local/"
+	};
+
 	public static String getMimeType(HttpServletRequest req, String supported) {
 		List<String> supportedList = Arrays.asList(StringUtils.split(supported, ','));
 		String mimeType = supportedList.get(0);
@@ -40,32 +47,34 @@ public class Utils {
 	}
 
 	public static List<String> getUsers() {
-		List<String> signingUsers = new ArrayList<>();
-		try {
-			HttpGet get = new HttpGet("http://grlc.io/api/peta-pico/nanopub-api/get_all_users?endpoint=http%3A%2F%2Fgraphdb.dumontierlab.com%2Frepositories%2Fnanopubs");
-			get.setHeader("Accept", "text/csv");
-			BufferedReader reader = null;
+		Set<String> users = new HashSet<>();
+		for (String apiUrl : apiInstances) {
 			try {
-				HttpResponse resp = httpClient.execute(get);
-				if (!wasSuccessful(resp)) {
-					EntityUtils.consumeQuietly(resp.getEntity());
-					throw new IOException(resp.getStatusLine().toString());
+				HttpGet get = new HttpGet(apiUrl + "get_all_users");
+				get.setHeader("Accept", "text/csv");
+				BufferedReader reader = null;
+				try {
+					HttpResponse resp = httpClient.execute(get);
+					if (!wasSuccessful(resp)) {
+						EntityUtils.consumeQuietly(resp.getEntity());
+						throw new IOException(resp.getStatusLine().toString());
+					}
+					reader = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+					String line = null;
+					int n = 0;
+					while ((line = reader.readLine()) != null) {
+						n++;
+						if (n == 1) continue; // skip header
+						users.add(line);
+					}
+				} finally {
+					if (reader != null) reader.close();
 				}
-				reader = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
-				String line = null;
-				int n = 0;
-				while ((line = reader.readLine()) != null) {
-					n++;
-					if (n == 1) continue; // skip header
-					signingUsers.add(line);
-				}
-			} finally {
-				if (reader != null) reader.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
 			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
 		}
-		return signingUsers;
+		return new ArrayList<String>(users);
 	}
 
 	private static boolean wasSuccessful(HttpResponse resp) {
