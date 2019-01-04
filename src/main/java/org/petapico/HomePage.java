6eccb233e6abb7807ee497798ed00ac1f20caea7
Tobias Kuhn <kuhntobias@gmail.com>
2019-01-04 16:07:56 +0100

    Clean-up code

--- src/main/java/org/petapico/HomePage.java
+++ src/main/java/org/petapico/HomePage.java
@@ -10,36 +10,25 @@ import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
-/**
- * Homepage
- */
 public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-	// TODO Add any page properties or variables here
-
-    /**
-	 * Constructor that is invoked when page is invoked without a session.
-	 * 
-	 * @param parameters
-	 *            Page parameters
-	 */
-    public HomePage(final PageParameters parameters) {
-    	List<String> users = ApiAccess.getAll("get_all_users", null, 0);
-        add(new DataView<String>("users", new ListDataProvider<String>(users)) {
+	public HomePage(final PageParameters parameters) {
+		List<String> users = ApiAccess.getAll("get_all_users", null, 0);
+		add(new DataView<String>("users", new ListDataProvider<String>(users)) {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
-        	protected void populateItem(Item<String> item) {
+			protected void populateItem(Item<String> item) {
 				PageParameters params = new PageParameters();
 				params.put("id", item.getModelObject());
 				BookmarkablePageLink<UserPage> l = new BookmarkablePageLink<UserPage>("userlink", UserPage.class, params);
 				l.add(new Label("linktext", item.getModelObject()));
 				item.add(l);
-        	}
+			}
 
-        });
-    }
+		});
+	}
 }
--- src/main/java/org/petapico/WicketApplication.java
+++ src/main/java/org/petapico/WicketApplication.java
@@ -2,25 +2,12 @@ package org.petapico;
 
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
- * @see org.petapico.Start#main(String[])
- */
-public class WicketApplication extends WebApplication
-{    
-    /**
-     * Constructor
-     */
-	public WicketApplication()
-	{
+public class WicketApplication extends WebApplication {
+
+	public WicketApplication() {
 	}
-	
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	public Class<HomePage> getHomePage()
-	{
+
+	public Class<HomePage> getHomePage() {
 		return HomePage.class;
 	}
 	
