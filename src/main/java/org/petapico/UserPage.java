7225a97d25eed4c0aec9eb9060318d558c0fdee8
Tobias Kuhn <kuhntobias@gmail.com>
2019-12-12 12:33:57 +0100

    Remember intro nanopubs

--- src/main/java/org/petapico/UserPage.java
+++ src/main/java/org/petapico/UserPage.java
@@ -24,6 +24,21 @@ public class UserPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
+	private static Map<String,IntroNanopub> introNanopubs = new HashMap<>();
+
+	public static IntroNanopub getIntroNanopub(String userId) {
+		IntroNanopub introNanopub = introNanopubs.get(userId);
+		if (introNanopub == null) {
+			try {
+				introNanopub = IntroNanopub.get(userId);
+				introNanopubs.put(userId, introNanopub);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return introNanopub;
+	}
+
 	private Model<String> progress;
 	private boolean nanopubsReady = false;
 
@@ -31,13 +46,7 @@ public class UserPage extends WebPage {
 		String userId = parameters.get("id").toString();
 		add(new Label("userid", userId));
 
-
-		IntroNanopub introNanopub = null;
-		try {
-			introNanopub = IntroNanopub.get(userId);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		IntroNanopub introNanopub = getIntroNanopub(userId);
 		List<KeyDeclaration> keyDeclarations;
 		if (introNanopub != null && introNanopub.getNanopub() != null) {
 			Nanopub np = introNanopub.getNanopub();
