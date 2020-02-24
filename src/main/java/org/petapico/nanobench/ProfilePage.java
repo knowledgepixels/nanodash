d9cf363d2cc65da9368a6b73b256c18f020fe00b
Tobias Kuhn <kuhntobias@gmail.com>
2020-02-24 16:30:54 +0100

    Remove debug messages

--- src/main/java/org/petapico/nanobench/ProfilePage.java
+++ src/main/java/org/petapico/nanobench/ProfilePage.java
@@ -240,9 +240,7 @@ public class ProfilePage extends WebPage {
 		if (userIri == null) {
 			if (orcidFile.exists()) {
 				try {
-					System.err.println("pORCID!");
 					String orcid = FileUtils.readFileToString(orcidFile, StandardCharsets.UTF_8).trim();
-					System.err.println("ORCID!");
 //					String orcid = Files.readString(orcidFile.toPath(), StandardCharsets.UTF_8).trim();
 					if (orcid.matches(ORCID_PATTERN)) {
 						userIri = vf.createIRI("https://orcid.org/" + orcid);
