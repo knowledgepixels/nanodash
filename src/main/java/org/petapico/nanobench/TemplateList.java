ef3c420d23aa97384065ebce873d6fba2957ea97
Tobias Kuhn <kuhntobias@gmail.com>
2020-02-24 10:45:16 +0100

    Sort templates

--- src/main/java/org/petapico/nanobench/TemplateList.java
+++ src/main/java/org/petapico/nanobench/TemplateList.java
@@ -1,7 +1,11 @@
 package org.petapico.nanobench;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -18,8 +22,20 @@ public class TemplateList extends Panel {
 
 	public TemplateList(String id) {
 		super(id);
+		List<Template> templateList = new ArrayList<>(Template.getTemplates());
+		Collections.sort(templateList, new Comparator<Template>() {
+			@Override
+			public int compare(Template t1, Template t2) {
+				Calendar c1 = getTime(t1);
+				Calendar c2 = getTime(t2);
+				if (c1 == null && c2 == null) return 0;
+				if (c1 == null) return 1;
+				if (c2 == null) return -1;
+				return c2.compareTo(c1);
+			}
+		});
 
-		add(new DataView<Template>("list", new ListDataProvider<Template>(Template.getTemplates())) {
+		add(new DataView<Template>("list", new ListDataProvider<Template>(templateList)) {
 
 			private static final long serialVersionUID = 1L;
 
@@ -29,16 +45,20 @@ public class TemplateList extends Panel {
 				params.add("template", item.getModelObject().getId());
 				BookmarkablePageLink<UserPage> l = new BookmarkablePageLink<UserPage>("link", PublishPage.class, params);
 				l.add(new Label("name", item.getModelObject().getLabel()));
+				item.add(l);
 				String timeString = "undated";
-				Calendar c = SimpleTimestampPattern.getCreationTime(item.getModelObject().getNanopub());
+				Calendar c = getTime(item.getModelObject());
 				if (c != null) {
 					timeString = (new SimpleDateFormat("yyyy-MM-dd")).format(c.getTime());
 				}
-				l.add(new Label("timestamp", timeString));
-				item.add(l);
+				item.add(new Label("timestamp", timeString));
 			}
 
 		});
+	}
+
+	private static Calendar getTime(Template template) {
+		return SimpleTimestampPattern.getCreationTime(template.getNanopub());
 	}
 
 }
