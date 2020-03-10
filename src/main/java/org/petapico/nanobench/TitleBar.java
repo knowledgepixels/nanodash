bd42a7358f88dfa1092875b145c08dd1f75f8e3b
Tobias Kuhn <kuhntobias@gmail.com>
2020-03-10 12:04:00 +0100

    Make all menu links work

--- src/main/java/org/petapico/nanobench/TitleBar.java
+++ src/main/java/org/petapico/nanobench/TitleBar.java
@@ -1,6 +1,8 @@
 package org.petapico.nanobench;
 
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class TitleBar extends Panel {
 
@@ -9,6 +11,13 @@ public class TitleBar extends Panel {
 	public TitleBar(String id) {
 		super(id);
 		add(new ProfileItem("profile"));
+		if (ProfilePage.getUserIri() != null) {
+			PageParameters params = new PageParameters();
+			params.add("id", ProfilePage.getUserIri());
+			add(new BookmarkablePageLink<UserPage>("mychannellink", UserPage.class, params));
+		} else {
+			add(new BookmarkablePageLink<UserPage>("mychannellink", ProfilePage.class));
+		}
 	}
 
 }
