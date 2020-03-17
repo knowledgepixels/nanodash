029bdca01641239b96c1c47e036510aa1e7575b7
Tobias Kuhn <kuhntobias@gmail.com>
2020-03-17 17:05:25 +0100

    Automatically open application in browser

--- src/main/java/org/petapico/nanobench/WicketApplication.java
+++ src/main/java/org/petapico/nanobench/WicketApplication.java
@@ -1,11 +1,34 @@
 package org.petapico.nanobench;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class WicketApplication extends WebApplication {
 
 	public WicketApplication() {
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+			try {
+				Desktop.getDesktop().browse(new URI("http://localhost:37373"));
+			} catch (IOException | URISyntaxException ex) {
+				ex.printStackTrace();
+			}
+		}
+		System.err.println("");
+		System.err.println("----------------------------------------");
+		System.err.println("               nanobench");
+		System.err.println("----------------------------------------");
+		System.err.println(" Your browser should show the nanobench");
+		System.err.println(" interface in a few seconds.");
+		System.err.println("");
+		System.err.println(" If not, point your browser to:");
+		System.err.println(" http://localhost:37373");
+		System.err.println("----------------------------------------");
+		System.err.println(""); 
 	}
 
 	public Class<HomePage> getHomePage() {
