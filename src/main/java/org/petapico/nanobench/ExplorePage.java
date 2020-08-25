c7ead79d3056c13c456e1ccff61060fcb88e311f
Tobias Kuhn <kuhntobias@gmail.com>
2020-08-25 17:00:53 +0200

    Increase limit for predicate frequency to create table

--- src/main/java/org/petapico/nanobench/ExplorePage.java
+++ src/main/java/org/petapico/nanobench/ExplorePage.java
@@ -32,7 +32,7 @@ public class ExplorePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final int maxDetailTableCount = 10000;
+	private static final int maxDetailTableCount = 1000000;
 
 	public ExplorePage(final PageParameters parameters) {
 		add(new TitleBar("titlebar"));
