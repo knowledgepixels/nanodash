ca3a0dcc9b69d9b8ebd00e98c644f25cde41e84b
Tobias Kuhn <kuhntobias@gmail.com>
2021-12-26 12:35:06 +0100

    Fix typo in Windows script

--- package/run-under-windows.bat
+++ package/run-under-windows.bat
@@ -1,5 +1,5 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
-if exists .extract rmdir /S /Q .extract
+if exist .extract rmdir /S /Q .extract
 
 java -jar nanobench.jar -httpPort 37373
