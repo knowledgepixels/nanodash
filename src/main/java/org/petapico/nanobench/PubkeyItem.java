c27815d4fd59337bd589ccc9aabe1dba7a4d602e
Tobias Kuhn <kuhntobias@gmail.com>
2022-07-22 16:08:51 +0200

    Improve display of key information

--- src/main/java/org/petapico/nanobench/PubkeyItem.java
+++ src/main/java/org/petapico/nanobench/PubkeyItem.java
@@ -10,11 +10,23 @@ public class PubkeyItem extends Panel {
 	public PubkeyItem(String id, String pubkey) {
 		super(id);
 
+		NanobenchSession session = NanobenchSession.get();
 		if (pubkey == null || pubkey.isEmpty()) {
 			add(new Label("label", ".."));
+			add(new Label("notes", ""));
 			add(new Label("fullkey", "(key does not exist)"));
 		} else {
 			add(new Label("label", Utils.getShortPubkeyLabel(pubkey)));
+			String notes = "";
+			if (pubkey.equals(session.getPubkeyString())) {
+				notes += "<strong>Local key:</strong> This is the key that is used to sign your nanopublications on this site.<br>";
+			}
+			if (User.getPubkeys(session.getUserIri(), true).contains(pubkey)) {
+				notes += "<strong class=\"positive\">Approved key:</strong> This key is approved by the community.<br>";
+			} else {
+				notes += "<strong class=\"negative\">Unapproved key:</strong> This key is so far not approved by the community.<br>";
+			}
+			add(new Label("notes", notes).setEscapeModelStrings(false));
 			add(new Label("fullkey", pubkey));
 		}
 	}
