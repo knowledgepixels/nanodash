6b421261fb97389ff46bf3138e6198d5e2d2f1ad
Tobias Kuhn <kuhntobias@gmail.com>
2022-08-01 20:26:48 +0200

    Add -Dfile.encoding=UTF-8 for Windows bat files

--- package/run-under-windows.bat
+++ package/run-under-windows.bat
@@ -2,4 +2,4 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 if exist .extract rmdir /S /Q .extract
 
-java -jar nanobench.jar -httpPort 37373
+java -jar nanobench.jar -httpPort 37373 -Dfile.encoding=UTF-8
--- package/update-under-windows.bat
+++ package/update-under-windows.bat
@@ -1,3 +1,3 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
-java -jar nanobench.jar -resetExtract -Dnanobench.run=update
+java -jar nanobench.jar -resetExtract -Dnanobench.run=update -Dfile.encoding=UTF-8
