37f3794ada68d0b42740e6edb5a6f6b496ed4592
Tobias Kuhn <kuhntobias@gmail.com>
2022-08-04 13:18:11 +0200

    Simplify key display on profile page

--- src/main/java/org/petapico/nanobench/PubkeyItem.java
+++ src/main/java/org/petapico/nanobench/PubkeyItem.java
@@ -14,20 +14,22 @@ public class PubkeyItem extends Panel {
 		if (pubkey == null || pubkey.isEmpty()) {
 			add(new Label("label", ".."));
 			add(new Label("notes", ""));
-			add(new Label("fullkey", "(key does not exist)"));
+			//add(new Label("fullkey", "(key does not exist)"));
 		} else {
 			add(new Label("label", Utils.getShortPubkeyName(pubkey)));
 			String notes = "";
 			if (pubkey.equals(session.getPubkeyString())) {
-				notes += "<strong>Local key:</strong> This is the key that is used to sign your nanopublications on this site.<br>";
+				notes += "This is the <strong>local key</strong> that is used to sign your nanopublications on this site. ";
+			} else {
+				notes += "This key is used on another site. ";
 			}
 			if (User.getPubkeys(session.getUserIri(), true).contains(pubkey)) {
-				notes += "<strong class=\"positive\">Approved key:</strong> This key is approved by the community.<br>";
+				notes += "It is <strong class=\"positive\">approved</strong> by the community.";
 			} else {
-				notes += "<strong class=\"negative\">Unapproved key:</strong> This key is so far not approved by the community.<br>";
+				notes += "It is so far <strong class=\"negative\">not approved</strong> by the community.";
 			}
 			add(new Label("notes", notes).setEscapeModelStrings(false));
-			add(new Label("fullkey", pubkey));
+			//add(new Label("fullkey", pubkey));
 		}
 	}
 
