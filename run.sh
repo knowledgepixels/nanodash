01b96579214e543eb1e164903dd5f0651ca281ab
Tobias Kuhn <kuhntobias@gmail.com>
2022-10-12 13:39:28 +0200

    Use latest nanopub library

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 cd "$(dirname "$0")"
 
+export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED"
+
 mvn clean tomcat7:run
