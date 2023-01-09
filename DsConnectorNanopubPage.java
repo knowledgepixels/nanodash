2a646f090c53bf701e768e04c24c7b7421d0b18d
Tobias Kuhn <kuhntobias@gmail.com>
2023-01-09 13:56:22 +0100

    Copy files from test connector

---
+++ DsConnectorNanopubPage.java
@@ -0,0 +1,35 @@
+package org.petapico.nanobench.connector.ios;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.nanopub.Nanopub;
+import org.petapico.nanobench.NanobenchSession;
+import org.petapico.nanobench.NanopubElement;
+import org.petapico.nanobench.NanopubItem;
+import org.petapico.nanobench.TitleBar;
+import org.petapico.nanobench.Utils;
+
+public class DsConnectorNanopubPage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String MOUNT_PATH = "/connector-ios-ds-np";
+
+	public DsConnectorNanopubPage(final PageParameters parameters) {
+		add(new TitleBar("titlebar"));
+		//add(new Label("titlebar"));  // hide title bar
+
+		final NanobenchSession session = NanobenchSession.get();
+		session.redirectToLoginIfNeeded(MOUNT_PATH, parameters);
+
+		final String ref = parameters.get("id").toString();
+
+		try {
+			Nanopub np = Utils.getAsNanopub(ref);
+			add(new NanopubItem("nanopub", new NanopubElement(np), false, false));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
---
+++ DsConnectorPage.java
@@ -0,0 +1,63 @@
+package org.petapico.nanobench.connector.ios;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.petapico.nanobench.ApiAccess;
+import org.petapico.nanobench.ApiResponse;
+import org.petapico.nanobench.ApiResponseEntry;
+import org.petapico.nanobench.NanobenchSession;
+import org.petapico.nanobench.TitleBar;
+
+import com.opencsv.exceptions.CsvValidationException;
+
+public class DsConnectorPage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String MOUNT_PATH = "/connector-ios-ds";
+
+	private static final String apiUrl = "https://grlc.petapico.org/api-git/knowledgepixels/connectortest-nanopub-api/";
+
+	public DsConnectorPage(final PageParameters parameters) {
+		add(new TitleBar("titlebar"));
+		//add(new Label("titlebar"));  // hide title bar
+
+		final NanobenchSession session = NanobenchSession.get();
+		session.redirectToLoginIfNeeded(MOUNT_PATH, parameters);
+
+		Map<String,String> params = new HashMap<>();
+		params.put("creator", session.getUserIri().stringValue());
+		try {
+			ApiResponse resp = ApiAccess.getAll(apiUrl, "get-formalization-nanopubs", params);
+	
+			add(new DataView<ApiResponseEntry>("formalization-nps", new ListDataProvider<ApiResponseEntry>(resp.getData())) {
+	
+				private static final long serialVersionUID = 1L;
+	
+				@Override
+				protected void populateItem(Item<ApiResponseEntry> item) {
+					ApiResponseEntry e = item.getModelObject();
+					PageParameters params = new PageParameters();
+					params.add("id", e.get("np"));
+					BookmarkablePageLink<WebPage> l = new BookmarkablePageLink<WebPage>("nplink", DsConnectorNanopubPage.class, params);
+					l.add(new Label("nplinktext", "\"" + e.get("label") + "\""));
+					item.add(l);
+				}
+	
+			});
+		} catch (IOException|CsvValidationException ex) {
+			// TODO Report error somehow...
+			add(new Label("formalization-nps"));
+		}
+	}
+
+}
