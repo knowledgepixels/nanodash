94ede99d0a537f40a4f108932d8c7e7ee2c62fe2
Tobias Kuhn <kuhntobias@gmail.com>
2023-01-31 16:34:38 +0100

    Improve pages

--- DsNanopubPage.java
+++ DsNanopubPage.java
@@ -20,6 +20,8 @@ import org.petapico.nanobench.ApiResponseEntry;
 import org.petapico.nanobench.ExplorePage;
 import org.petapico.nanobench.NanopubElement;
 import org.petapico.nanobench.NanopubItem;
+import org.petapico.nanobench.PublishPage;
+import org.petapico.nanobench.Template;
 import org.petapico.nanobench.TitleBar;
 import org.petapico.nanobench.User;
 import org.petapico.nanobench.Utils;
@@ -42,6 +44,15 @@ public class DsNanopubPage extends WebPage {
 			String uri = np.getUri().stringValue();
 			String shortId = "np:" + Utils.getShortNanopubId(uri);
 
+			Template template = Template.getTemplate(np);
+			if (template == null) {
+				add(new Label("template-name", "(none)"));
+				add(new Label("template-description", ""));
+			} else {
+				add(new Label("template-name", template.getLabel()));
+				add(new Label("template-description", (template.getDescription() == null ? "" : template.getDescription())).setEscapeModelStrings(false));
+			}
+
 			add(new Image("form-submit", new PackageResourceReference(this.getClass(), "DsFormSubmit.png")));
 
 			add(new ExternalLink("np-link", uri, uri));
@@ -50,7 +61,7 @@ public class DsNanopubPage extends WebPage {
 			add(new Label("latex-np-label", shortId.replace("_", "\\_")));
 
 			Map<String,String> params = new HashMap<>();
-			params.put("paper", uri);
+			params.put("pub", uri);
 			ApiResponse resp = ApiAccess.getAll(FcOverviewPage.apiUrl, "get-reactions", params);
 	
 			add(new DataView<ApiResponseEntry>("reactions", new ListDataProvider<ApiResponseEntry>(resp.getData())) {
@@ -61,14 +72,25 @@ public class DsNanopubPage extends WebPage {
 				protected void populateItem(Item<ApiResponseEntry> item) {
 					ApiResponseEntry e = item.getModelObject();
 					PageParameters params = new PageParameters();
+					if (e.get("pub").equals(np.getUri().stringValue())) {
+						item.add(new Label("reactionnote"));
+					} else {
+						item.add(new Label("reactionnote", "On earlier version:"));
+					}
 					params.add("id", e.get("np"));
 					BookmarkablePageLink<WebPage> l = new BookmarkablePageLink<WebPage>("reaction", ExplorePage.class, params);
 					String username = User.getShortDisplayNameForPubkey(e.get("pubkey"));
-					l.add(new Label("reactiontext", "\"" + e.get("text") + "\" by " + username + " on " + e.get("date").substring(0, 10)));
+					l.add(new Label("reactiontext", "\"" + e.get("text") + "\" (" + e.get("reltext") + ") by " + username + " on " + e.get("date").substring(0, 10)));
 					item.add(l);
 				}
 	
 			});
+
+			add(new BookmarkablePageLink<WebPage>("create-new-reaction", PublishPage.class,
+					new PageParameters()
+						.add("template", "http://purl.org/np/RA4qeqqwcQGKQX9AgSd_3nNzECBYsohceseJ5FdFU_kjQ")
+						.add("param_paper", np.getUri().stringValue())
+						.add("template-version", "latest")));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
