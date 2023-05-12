942d8680194251ab258ea5eae3b61aa2d05ae017
Tobias Kuhn <kuhntobias@gmail.com>
2023-05-12 20:34:28 +0200

    Add link to RIO connector page

--- RioOverviewPage.java
+++ RioOverviewPage.java
@@ -2,14 +2,20 @@ package com.knowledgepixels.nanodash.connector.pensoft;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.knowledgepixels.nanodash.ConnectorListPage;
 import com.knowledgepixels.nanodash.connector.base.ConnectorConfig;
 import com.knowledgepixels.nanodash.connector.base.OverviewPage;
+import com.knowledgepixels.nanodash.connector.ios.DsOverviewPage;
 
 public class RioOverviewPage extends OverviewPage {
 
 	private static final long serialVersionUID = 1L;
 
 	public static final String MOUNT_PATH = "/connector/pensoft/rio";
+
+	static {
+		ConnectorListPage.addConnector(RioOverviewPage.class, "Nanopublishing in RIO Journal at Pensoft");
+	}
 
 	public RioOverviewPage(PageParameters params) {
 		super(params);
