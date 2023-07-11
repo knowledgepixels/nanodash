a4698559c97a98bf56ebe9abd4ad7301bc05e786
Tobias Kuhn <kuhntobias@gmail.com>
2023-07-11 14:21:05 +0200

    Refactor code to make it more concise

--- src/main/java/com/knowledgepixels/nanodash/TitleBar.java
+++ src/main/java/com/knowledgepixels/nanodash/TitleBar.java
@@ -11,17 +11,9 @@ public class TitleBar extends Panel {
 		super(id);
 		add(new ProfileItem("profile", page));
 
-		WebMarkupContainer mychannel = new WebMarkupContainer("mychannel");
-		mychannel.setVisible(!NanodashPreferences.get().isReadOnlyMode());
-		add(mychannel);
-
-		WebMarkupContainer connectors = new WebMarkupContainer("connectors");
-		connectors.setVisible(ConnectorListPage.getConnectorCount() > 0);
-		add(connectors);
-
-		WebMarkupContainer publish = new WebMarkupContainer("publish");
-		publish.setVisible(!NanodashPreferences.get().isReadOnlyMode());
-		add(publish);
+		add(new WebMarkupContainer("mychannel").setVisible(!NanodashPreferences.get().isReadOnlyMode()));
+		add(new WebMarkupContainer("connectors").setVisible(ConnectorListPage.getConnectorCount() > 0));
+		add(new WebMarkupContainer("publish").setVisible(!NanodashPreferences.get().isReadOnlyMode()));
 	}
 
 }
