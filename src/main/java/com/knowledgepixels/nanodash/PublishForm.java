3a0defca4c226913be85d8c9d29db36ccc049839
Tobias Kuhn <kuhntobias@gmail.com>
2023-08-15 15:25:45 +0200

    Support prefixes for label pattern processing

--- src/main/java/com/knowledgepixels/nanodash/PublishForm.java
+++ src/main/java/com/knowledgepixels/nanodash/PublishForm.java
@@ -690,8 +690,10 @@ public class PublishForm extends Panel {
 			String placeholderLabel = placeholderValue;
 			if (assertionContext.getTemplate().isUriPlaceholder(placeholderIri)) {
 				try {
-					// TODO Fix this. It doesn't work for placeholders with prefixes, auto-encode placeholders, etc.
+					// TODO Fix this. It doesn't work for placeholders with auto-encode placeholders, etc.
 					//      Not sure we need labels for these, but this code should be improved anyway.
+					String prefix = assertionContext.getTemplate().getPrefix(placeholderIri);
+					if (prefix != null) placeholderValue = prefix + placeholderValue;
 					IRI placeholderValueIri = vf.createIRI(placeholderValue);
 					String l = assertionContext.getTemplate().getLabel(placeholderValueIri);
 					if (l == null) l = GuidedChoiceItem.getLabel(placeholderValue);
