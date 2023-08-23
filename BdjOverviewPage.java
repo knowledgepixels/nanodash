7d7dbfe5757a746bc79cd454d341ebeb5a042d22
Tobias Kuhn <kuhntobias@gmail.com>
2023-08-23 15:47:27 +0200

    Add candidate list

--- BdjOverviewPage.java
+++ BdjOverviewPage.java
@@ -1,7 +1,20 @@
 package com.knowledgepixels.nanodash.connector.pensoft;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.nanopub.extra.services.ApiResponse;
+import org.nanopub.extra.services.ApiResponseEntry;
+
+import com.knowledgepixels.nanodash.ExplorePage;
+import com.knowledgepixels.nanodash.User;
 import com.knowledgepixels.nanodash.connector.base.ConnectorConfig;
 import com.knowledgepixels.nanodash.connector.base.OverviewPage;
 
@@ -13,6 +26,35 @@ public class BdjOverviewPage extends OverviewPage {
 
 	public BdjOverviewPage(PageParameters params) {
 		super(params);
+
+		try {
+			ApiResponse resp = callApi("get-biodiv-nanopubs", new HashMap<>());
+
+			ArrayList<ApiResponseEntry> respList = new ArrayList<>();
+			for (ApiResponseEntry a : resp.getData()) {
+				if (respList.size() == 10) break;
+				respList.add(a);
+			}
+	
+			add(new DataView<ApiResponseEntry>("candidates", new ListDataProvider<ApiResponseEntry>(respList)) {
+	
+				private static final long serialVersionUID = 1L;
+	
+				@Override
+				protected void populateItem(Item<ApiResponseEntry> item) {
+					ApiResponseEntry e = item.getModelObject();
+					PageParameters params = new PageParameters().add("id", e.get("np"));
+					BookmarkablePageLink<WebPage> l = new BookmarkablePageLink<WebPage>("candidatelink", ExplorePage.class, params);
+					l.add(new Label("candidatelinktext", "\"" +  e.get("label") + "\""));
+					item.add(l);
+					String username = User.getShortDisplayNameForPubkey(e.get("pubkey"));
+					item.add(new Label("candidatenote", "by " + username + " on " + e.get("date").substring(0, 10)));
+				}
+	
+			});
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	@Override
