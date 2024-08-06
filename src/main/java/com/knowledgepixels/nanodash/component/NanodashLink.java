cb65ff79f6098a7f4567854bd93802654c65486d
Tobias Kuhn <kuhntobias@gmail.com>
2024-08-06 13:47:40 +0200

    Add icons

--- src/main/java/com/knowledgepixels/nanodash/component/NanodashLink.java
+++ src/main/java/com/knowledgepixels/nanodash/component/NanodashLink.java
@@ -12,7 +12,6 @@ import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.ExternalLink;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
