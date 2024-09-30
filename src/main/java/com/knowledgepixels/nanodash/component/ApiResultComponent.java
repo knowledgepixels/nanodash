802b963706c242bb658fe39ac3a5b7afceb777c9
Tobias Kuhn <kuhntobias@gmail.com>
2024-09-30 15:26:16 +0200

    Add ApiResultComponent class (not yet used)

---
+++ src/main/java/com/knowledgepixels/nanodash/component/ApiResultComponent.java
@@ -0,0 +1,46 @@
+package com.knowledgepixels.nanodash.component;
+
+import java.util.HashMap;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxLazyLoadPanel;
+import org.nanopub.extra.services.ApiResponse;
+
+import com.knowledgepixels.nanodash.ApiCache;
+
+
+// TODO This class is not yet used.
+
+public abstract class ApiResultComponent extends AjaxLazyLoadPanel<Component> {
+
+	private static final long serialVersionUID = 1L;
+
+	private final String queryName;
+	private final HashMap<String,String> params;
+
+	public ApiResultComponent(String id, String queryName, HashMap<String,String> params) {
+		super(id);
+		this.queryName = queryName;
+		this.params = params;
+	}
+
+	@Override
+	public Component getLazyLoadComponent(String markupId) {
+		ApiResponse r = null;
+		while (true) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			if (!ApiCache.isRunning(queryName, params)) {
+				r = ApiCache.retrieveResponse(queryName, params);
+				if (r != null) break;
+			}
+		}
+		return getApiResultComponent(markupId, r);
+	}
+
+	public abstract Component getApiResultComponent(String markupId, ApiResponse response);
+
+}
