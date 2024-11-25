a31bce28863a63ce5ab19452318fb4d1b3d2f8a4
Tobias Kuhn <kuhntobias@gmail.com>
2024-11-25 13:22:43 +0100

    Fix null-pointer problem for unsigned nanopubs on Explore page

--- src/main/java/com/knowledgepixels/nanodash/component/NanopubItem.java
+++ src/main/java/com/knowledgepixels/nanodash/component/NanopubItem.java
@@ -80,7 +80,7 @@ public class NanopubItem extends Panel {
 			if (!hideActionMenu && (actions == null || !actions.isEmpty())) {
 				NanodashSession session = NanodashSession.get();
 				final boolean isOwnNanopub = (session.getUserIri() != null && session.getUserIri().equals(User.getUserData().getUserIri(pubkey, false))) ||
-						pubkey.equals(session.getPubkeyString());
+						((pubkey != null) && pubkey.equals(session.getPubkeyString()));
 				final boolean hasLocalPubkey = session.getUserIri() != null && session.getPubkeyString() != null && session.getPubkeyString().equals(pubkey);
 				final List<NanopubAction> actionList = new ArrayList<>();
 				final Map<String,NanopubAction> actionMap = new HashMap<>();
