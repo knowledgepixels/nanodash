1aaa8d3a8fe7dd94195e811260a4749d011453a0
Tobias Kuhn <kuhntobias@gmail.com>
2024-12-02 13:59:36 +0100

    Redirect terms to plain Explore page (for now)

--- src/main/java/com/knowledgepixels/nanodash/page/TermForwarder.java
+++ src/main/java/com/knowledgepixels/nanodash/page/TermForwarder.java
@@ -1,20 +1,8 @@
 package com.knowledgepixels.nanodash.page;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.wicket.request.flow.RedirectToUrlException;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.ValueFactory;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-import org.nanopub.extra.services.ApiResponse;
-import org.nanopub.extra.services.ApiResponseEntry;
 
-import com.knowledgepixels.nanodash.QueryApiAccess;
-import com.knowledgepixels.nanodash.User;
 import com.knowledgepixels.nanodash.Utils;
 
 public class TermForwarder extends NanodashPage {
@@ -34,30 +22,38 @@ public class TermForwarder extends NanodashPage {
 		//
 		//System.err.println("P: " + ((HttpServletRequest) getRequest().getContainerRequest()).getHeader("Accept"));
 
-		IRI id = vf.createIRI(parameters.get("id").toString());
-		IRI authority = null;
-		if (!parameters.get("authority").isEmpty()) {
-			authority = vf.createIRI(parameters.get("authority").toString());
-		}
-		Map<String,String> params = new HashMap<>();
-		params.put("thing", id.stringValue());
-		ApiResponse dataResponse = QueryApiAccess.get("get-introducing-nanopub", params);
-		List<ApiResponseEntry> responses;
-		if (authority == null) {
-			responses = dataResponse.getData();
-		} else {
-			responses = new ArrayList<>();
-			for (ApiResponseEntry r : dataResponse.getData()) {
-				if (authority.equals(User.getUserIri(r.get("pubkey")))) responses.add(r);
-			}
-		}
-		if (responses.size() != 1) {
-			throw new RedirectToUrlException(Utils.getUrlWithParameters(SearchPage.MOUNT_PATH, new PageParameters().add("query", id.stringValue())));
-		}
-		String npUri = responses.get(0).get("np");
-		throw new RedirectToUrlException(Utils.getUrlWithParameters(ExplorePage.MOUNT_PATH, new PageParameters().add("id", npUri)));
+		// Earlier code to redirect to nanopub if authority matches:
+
+//		IRI id = vf.createIRI(parameters.get("id").toString());
+//		IRI authority = null;
+//		if (!parameters.get("authority").isEmpty()) {
+//			authority = vf.createIRI(parameters.get("authority").toString());
+//		}
+//		Map<String,String> params = new HashMap<>();
+//		params.put("thing", id.stringValue());
+//		ApiResponse dataResponse = QueryApiAccess.get("get-introducing-nanopub", params);
+//		List<ApiResponseEntry> responses;
+//		if (authority == null) {
+//			responses = dataResponse.getData();
+//		} else {
+//			responses = new ArrayList<>();
+//			for (ApiResponseEntry r : dataResponse.getData()) {
+//				if (authority.equals(User.getUserIri(r.get("pubkey")))) responses.add(r);
+//			}
+//		}
+//		if (responses.size() != 1) {
+//			throw new RedirectToUrlException(Utils.getUrlWithParameters(SearchPage.MOUNT_PATH, new PageParameters().add("query", id.stringValue())));
+//		}
+//		String npUri = responses.get(0).get("np");
+//		throw new RedirectToUrlException(Utils.getUrlWithParameters(ExplorePage.MOUNT_PATH, new PageParameters().add("id", npUri)));
+
+		// Currently just redirecting to explore page, where "authority" param is ignored:
+
+		throw new RedirectToUrlException(Utils.getUrlWithParameters(ExplorePage.MOUNT_PATH, parameters));
+
+		// TODO Consider authority again, and show matching (or non-matching) authority accordingly
 	}
 
-	private static final ValueFactory vf = SimpleValueFactory.getInstance();
+//	private static final ValueFactory vf = SimpleValueFactory.getInstance();
 
 }
