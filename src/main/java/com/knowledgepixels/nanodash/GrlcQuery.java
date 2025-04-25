328aa8bcbeb14830d0474189b2139bdf556e1d50
Tobias Kuhn <kuhntobias@gmail.com>
2025-04-25 17:33:44 +0200

    Create GrlcQuery class

---
+++ src/main/java/com/knowledgepixels/nanodash/GrlcQuery.java
@@ -0,0 +1,147 @@
+package com.knowledgepixels.nanodash;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.vocabulary.DCTERMS;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.algebra.Var;
+import org.eclipse.rdf4j.query.algebra.helpers.AbstractSimpleQueryModelVisitor;
+import org.eclipse.rdf4j.query.parser.ParsedQuery;
+import org.eclipse.rdf4j.query.parser.sparql.SPARQLParser;
+import org.nanopub.Nanopub;
+
+import net.trustyuri.TrustyUriUtils;
+
+
+public class GrlcQuery implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public final static IRI GRLC_QUERY_CLASS = Utils.vf.createIRI("https://w3id.org/kpxl/grlc/grlc-query");
+	public final static IRI GRLC_HAS_SPARQL = Utils.vf.createIRI("https://w3id.org/kpxl/grlc/sparql");
+	public final static IRI GRLC_HAS_ENDPOINT = Utils.vf.createIRI("https://w3id.org/kpxl/grlc/endpoint");
+
+	private final String queryId;
+	private final String artifactCode;
+	private final String querySuffix;
+	private final Nanopub nanopub;
+	private IRI queryUri;
+	private String sparql;
+	private IRI endpoint;
+	private String label;
+	private String description;
+	private final List<String> placeholdersList;
+
+	public GrlcQuery(String id) {
+		if (id == null) {
+			throw new IllegalArgumentException("Null value for query ID");
+		}
+		if (TrustyUriUtils.isPotentialTrustyUri(id)) {
+			artifactCode = TrustyUriUtils.getArtifactCode(id);
+			nanopub = Utils.getNanopub(artifactCode);
+			for (Statement st : nanopub.getAssertion()) {
+				if (st.getPredicate().equals(RDF.TYPE) && st.getObject().equals(GRLC_QUERY_CLASS)) {
+					if (queryUri != null) {
+						throw new IllegalArgumentException("Nanopublication defines more than one query: " + id);
+					}
+					queryUri = (IRI) st.getSubject();
+				}
+			}
+			if (queryUri == null) {
+				throw new IllegalArgumentException("No query found in nanopublication: " + id);
+			}
+			queryId = queryUri.stringValue().replaceFirst("^https?://.*[^A-Za-z0-9-_](RA[^A-Za-z0-9-_]{43}[/#][^/#]+)$", "$1").replace("#", "/");
+		} else {
+			if (id.matches("https?://.*[^A-Za-z0-9-_]RA[^A-Za-z0-9-_]{43}[/#][^/#]+")) {
+				queryId = id.replaceFirst("^https?://.*[^A-Za-z0-9-_](RA[^A-Za-z0-9-_]{43}[/#][^/#]+)$", "$1").replace("#", "/");
+			} else if (id.matches(id)) {
+				queryId = id.replace("#", "/");
+			} else {
+				throw new IllegalArgumentException("Not a valid query ID or URI: " + id);
+			}
+			artifactCode = queryId.replaceFirst("[/#].*$", "");
+			nanopub = Utils.getNanopub(artifactCode);
+		}
+		querySuffix = queryId.replaceFirst("^.*[/#]", "");
+		for (Statement st : nanopub.getAssertion()) {
+			if (!st.getSubject().stringValue().replace("#", "/").equals(queryId)) continue;
+			queryUri = (IRI) st.getSubject();
+			if (st.getPredicate().equals(GRLC_HAS_SPARQL) && st.getObject() instanceof Literal objLiteral) {
+				sparql = objLiteral.stringValue();
+			} else if (st.getPredicate().equals(GRLC_HAS_ENDPOINT) && st.getObject() instanceof IRI objIri) {
+				endpoint = objIri;
+			} else if (st.getPredicate().equals(RDFS.LABEL)) {
+				label = st.getObject().stringValue();
+			} else if (st.getPredicate().equals(DCTERMS.DESCRIPTION)) {
+				description = st.getObject().stringValue();
+			}
+		}
+
+		final Set<String> placeholders = new HashSet<>();
+		ParsedQuery query = new SPARQLParser().parseQuery(sparql, null);
+		query.getTupleExpr().visitChildren(new AbstractSimpleQueryModelVisitor<>() {
+
+			@Override
+			public void meet(Var node) throws RuntimeException {
+				super.meet(node);
+				if (!node.isConstant() && !node.isAnonymous() && node.getName().startsWith("_")) {
+					placeholders.add(node.getName());
+				}
+			}
+
+		});
+		List<String> placeholdersListPre = new ArrayList<>(placeholders);
+		Collections.sort(placeholdersListPre);
+		placeholdersList = Collections.unmodifiableList(placeholdersListPre);
+	}
+
+	public String getQueryId() {
+		return queryId;
+	}
+
+	public String getArtifactCode() {
+		return artifactCode;
+	}
+
+	public String getQuerySuffix() {
+		return querySuffix;
+	}
+
+	public Nanopub getNanopub() {
+		return nanopub;
+	}
+
+	public IRI getQueryUri() {
+		return queryUri;
+	}
+
+	public String getSparql() {
+		return sparql;
+	}
+
+	public IRI getEndpoint() {
+		return endpoint;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public List<String> getPlaceholdersList() {
+		return placeholdersList;
+	}
+
+}
--- src/main/java/com/knowledgepixels/nanodash/page/QueryPage.java
+++ src/main/java/com/knowledgepixels/nanodash/page/QueryPage.java
@@ -1,24 +1,13 @@
 package com.knowledgepixels.nanodash.page;
 
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.query.algebra.Var;
-import org.eclipse.rdf4j.query.algebra.helpers.AbstractSimpleQueryModelVisitor;
-import org.eclipse.rdf4j.query.parser.ParsedQuery;
-import org.eclipse.rdf4j.query.parser.sparql.SPARQLParser;
-import org.nanopub.Nanopub;
 
 import com.github.jsonldjava.shaded.com.google.common.base.Charsets;
-import com.knowledgepixels.nanodash.Utils;
+import com.knowledgepixels.nanodash.GrlcQuery;
 import com.knowledgepixels.nanodash.component.TitleBar;
 
 public class QueryPage extends NanodashPage {
@@ -38,60 +27,19 @@ public class QueryPage extends NanodashPage {
 		add(new Label("pagetitle", "Query Info | nanodash"));
 
 		String npId = parameters.get("id").toString();
-		String queryString = getQueryString(npId);
-		List<String> placeholders = getQueryPlaceholders(queryString);
+		GrlcQuery q = new GrlcQuery(npId);
 		String placeholdersString = "";
-		for (String p : placeholders) {
+		for (String p : q.getPlaceholdersList()) {
 			placeholdersString += p + " ";
 		}
 		add(new Label("placeholders", placeholdersString));
-		String endpoint = getQueryEndpoint(npId);
 		// TODO Replace hard-coded Nanopub Query URL with dynamic solution:
-		String editLink = endpoint.replaceFirst("^.*/repo/", "https://query.petapico.org/tools/") + "/yasgui.html#query=" + URLEncoder.encode(queryString, Charsets.UTF_8);
+		String editLink = q.getEndpoint().stringValue().replaceFirst("^.*/repo/", "https://query.petapico.org/tools/") + "/yasgui.html#query=" + URLEncoder.encode(q.getSparql(), Charsets.UTF_8);
 		add(new ExternalLink("editlink", editLink));
 	}
 
 	protected boolean hasAutoRefreshEnabled() {
 		return true;
-	}
-
-	public static String getQueryString(String npId) {
-		Nanopub np = Utils.getNanopub(npId);
-		for (Statement st : np.getAssertion()) {
-			if (st.getPredicate().stringValue().equals("https://w3id.org/kpxl/grlc/sparql")) {
-				return st.getObject().stringValue();
-			}
-		}
-		return null;
-	}
-
-	public static String getQueryEndpoint(String npId) {
-		Nanopub np = Utils.getNanopub(npId);
-		for (Statement st : np.getAssertion()) {
-			if (st.getPredicate().stringValue().equals("https://w3id.org/kpxl/grlc/endpoint")) {
-				return st.getObject().stringValue();
-			}
-		}
-		return null;
-	}
-
-	public static List<String> getQueryPlaceholders(String queryString) {
-		final Set<String> placeholders = new HashSet<>();
-		ParsedQuery query = new SPARQLParser().parseQuery(queryString, null);
-		query.getTupleExpr().visitChildren(new AbstractSimpleQueryModelVisitor<>() {
-
-			@Override
-			public void meet(Var node) throws RuntimeException {
-				super.meet(node);
-				if (!node.isConstant() && node.getName().startsWith("_")) {
-					placeholders.add(node.getName());
-				}
-			}
-
-		});
-		final List<String> placeholdersList = new ArrayList<>(placeholders);
-		Collections.sort(placeholdersList);
-		return placeholdersList;
 	}
 
 }
