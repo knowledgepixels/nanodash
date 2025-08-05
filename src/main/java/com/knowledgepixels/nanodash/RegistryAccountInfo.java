7a3f35e21e0a605fc19b8a081fd0c6f65e1f208b
Tobias Kuhn <kuhntobias@gmail.com>
2025-08-05 11:57:03 +0200

    Add RegistryAccountInfo class (not yet used)

---
+++ src/main/java/com/knowledgepixels/nanodash/RegistryAccountInfo.java
@@ -0,0 +1,72 @@
+package com.knowledgepixels.nanodash;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import com.github.jsonldjava.shaded.com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * This class is not yet used.
+ *
+ * It can access registry account lists of Nanopub Registry instances.
+ */
+public class RegistryAccountInfo {
+
+	private static Gson g = new Gson();
+	private static Type listType = new TypeToken<ArrayList<RegistryAccountInfo>>(){}.getType();
+
+	/**
+	 * @param url e.g. "https://registry.knowledgepixels.com/list.json"
+	 */
+	public static ArrayList<RegistryAccountInfo> fromUrl(String url) throws JsonIOException, JsonSyntaxException, MalformedURLException, IOException {
+		return g.fromJson(new InputStreamReader(new URL(url).openStream()), listType);
+	}
+
+	private String agent;
+	private String pubkey;
+	private String status;
+	private int depth;
+	private int pathCount;
+	private long quota;
+	private double ratio;
+
+	public static Type getListType() {
+		return listType;
+	}
+
+	public String getAgent() {
+		return agent;
+	}
+
+	public String getPubkey() {
+		return pubkey;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public int getPathCount() {
+		return pathCount;
+	}
+
+	public long getQuota() {
+		return quota;
+	}
+
+	public double getRatio() {
+		return ratio;
+	}
+
+}
