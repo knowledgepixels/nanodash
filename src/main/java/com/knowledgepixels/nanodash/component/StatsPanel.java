e49d9ec4f47743c5139fc997fca12fb8d52d5a66
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-21 12:43:49 +0200

    chore(StatsPanel): remove duplicate label creation for valid NP count

--- src/main/java/com/knowledgepixels/nanodash/component/StatsPanel.java
+++ src/main/java/com/knowledgepixels/nanodash/component/StatsPanel.java
@@ -32,7 +32,6 @@ public class StatsPanel extends Panel {
         this.statsMap = statsMap;
         setOutputMarkupId(true);
 
-        add(new Label("latestcount", getLong("validNpCount")));
         add(new Label("latestcount", getLong(VALID_NP_COUNT_KEY)));
         add(new Label("previouscount", getLong(INVALIDATED_NP_COUNT_KEY)));
         add(new Label("acceptedcount", getLong(ACCEPTED_NP_COUNT_KEY)));
