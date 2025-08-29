22991246470e61f9a3901bad981485af503cb8aa
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-29 09:52:44 +0200

    chore: synchronize user data refresh method

--- src/main/java/com/knowledgepixels/nanodash/User.java
+++ src/main/java/com/knowledgepixels/nanodash/User.java
@@ -23,8 +23,8 @@ public class User {
     /**
      * Refreshes the user data by creating a new UserData instance.
      */
-    public static void refreshUsers() {
-        if (userData == null || System.currentTimeMillis() - lastRefresh > REFRESH_INTERVAL) {
+    public synchronized static void refreshUsers() {
+        if (System.currentTimeMillis() - lastRefresh > REFRESH_INTERVAL) {
             lastRefresh = System.currentTimeMillis();
             userData = new UserData();
         }
