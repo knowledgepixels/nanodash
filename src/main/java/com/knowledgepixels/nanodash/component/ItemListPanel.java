072fc5b33c50cca2d357a9577a78f7875d08f018
Tobias Kuhn <kuhntobias@gmail.com>
2025-09-09 12:58:59 +0200

    Generalize lazy loading of ItemListPanel

--- src/main/java/com/knowledgepixels/nanodash/component/ItemListPanel.java
+++ src/main/java/com/knowledgepixels/nanodash/component/ItemListPanel.java
@@ -1,10 +1,12 @@
 package com.knowledgepixels.nanodash.component;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.markup.html.navigation.paging.AjaxPagingNavigator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.NavigatorLabel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -13,14 +15,22 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.nanopub.extra.services.ApiResponse;
+
+import com.knowledgepixels.nanodash.ApiCache;
 
 public class ItemListPanel<T extends Serializable> extends Panel {
 
-    public ItemListPanel(String id, String title, String description, List<T> items, ComponentProvider<T> compProvider) {
+    public ItemListPanel(String id, String title, List<T> items, ComponentProvider<T> compProvider) {
         super(id);
         setOutputMarkupId(true);
+        if (title.contains("  ")) {
+            add(new Label("description", title.replaceFirst("^.*  ", "")));
+            title = title.replaceFirst("  .*$", "");
+        } else {
+            add(new Label("description").setVisible(false));
+        }
         add(new Label("title", title));
-        add(new Label("description", description));
         add(new Label("button").setVisible(false));
 
         DataView<T> dataView = new DataView<T>("itemlist", new ListDataProvider<T>(items)) {
@@ -44,6 +54,45 @@ public class ItemListPanel<T extends Serializable> extends Panel {
     }
 
     public static interface ComponentProvider<T> extends Function<T, Component>, Serializable {
+    }
+
+    public static interface ApiResultListProvider<T> extends Function<ApiResponse, List<T>>, Serializable {
+    }
+
+    public static class LazyLoad<T extends Serializable> implements Serializable {
+
+        private String markupId;
+        private String queryName;
+        private HashMap<String, String> params;
+        private ApiResultListProvider<T> resultListProvider;
+        private String title;
+        private ComponentProvider<T> compProvider;
+
+        public LazyLoad(String markupId, String title, String queryName, HashMap<String, String> params, ApiResultListProvider<T> resultListProvider, ComponentProvider<T> compProvider) {
+            this.markupId = markupId;
+            this.queryName = queryName;
+            this.params = params;
+            this.resultListProvider = resultListProvider;
+            this.title = title;
+            this.compProvider = compProvider;
+        }
+
+        public MarkupContainer getContainer() {
+            ApiResponse qResponse = ApiCache.retrieveResponse(queryName, params);
+            if (qResponse != null) {
+                return new ItemListPanel<T>(markupId, title, resultListProvider.apply(qResponse), compProvider);
+            } else {
+                return new ApiResultComponent(markupId, queryName, params) {
+
+                    @Override
+                    public Component getApiResultComponent(String markupId, ApiResponse response) {
+                        return new ItemListPanel<T>(markupId, title, resultListProvider.apply(response), compProvider);
+                    }
+                };
+
+            }
+        }
+        
     }
 
 }
--- src/main/java/com/knowledgepixels/nanodash/component/ProjectList.java
+++ src/main/java/com/knowledgepixels/nanodash/component/ProjectList.java
@@ -2,14 +2,11 @@ package com.knowledgepixels.nanodash.component;
 
 import java.util.HashMap;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.nanopub.extra.services.ApiResponse;
 
-import com.knowledgepixels.nanodash.ApiCache;
 import com.knowledgepixels.nanodash.Project;
 import com.knowledgepixels.nanodash.page.ProjectPage;
 
@@ -20,30 +17,18 @@ import com.knowledgepixels.nanodash.page.ProjectPage;
 public class ProjectList {
 
     public static MarkupContainer getListContainer(String markupId) {
-        final HashMap<String, String> noParams = new HashMap<>();
-        final String queryName = "get-projects";
-        ApiResponse qResponse = ApiCache.retrieveResponse(queryName, noParams);
-        if (qResponse != null) {
-            return createComponent(markupId, qResponse);
-        } else {
-            return new ApiResultComponent(markupId, queryName, noParams) {
-
-                @Override
-                public Component getApiResultComponent(String markupId, ApiResponse response) {
-                    return createComponent(markupId, response);
+        return new ItemListPanel.LazyLoad<Project>(
+                markupId,
+                "Projects  Project pages are still experimental:",
+                "get-projects",
+                new HashMap<>(),
+                (resp) -> { Project.refresh(resp); return Project.getProjectList(); },
+                (p) -> {
+                    PageParameters params = new PageParameters();
+                    params.add("id", p.getId());
+                    return new BookmarkablePageLink<>("item", ProjectPage.class, params).setBody(Model.of(p.getLabel()));
                 }
-            };
-
-        }
-    }
-
-    private static ItemListPanel<Project> createComponent(String id, ApiResponse resp) {
-        Project.refresh(resp);
-        return new ItemListPanel<Project>(id, "Projects", "Project pages are still experimental:", Project.getProjectList(), (p) -> {
-                PageParameters params = new PageParameters();
-                params.add("id", p.getId());
-                return new BookmarkablePageLink<>("item", ProjectPage.class, params).setBody(Model.of(p.getLabel()));
-            });
+            ).getContainer();
     }
 
 }
