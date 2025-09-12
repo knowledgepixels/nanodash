d7481ada933935a3de16b0ace5e09bb664eecc1d
Tobias Kuhn <kuhntobias@gmail.com>
2025-09-12 13:14:57 +0200

    fix(Spaces): Fix pinned template view with new PinGroupList

--- src/main/java/com/knowledgepixels/nanodash/QueryApiAccess.java
+++ src/main/java/com/knowledgepixels/nanodash/QueryApiAccess.java
@@ -72,7 +72,7 @@ public class QueryApiAccess {
         load("RApiw7Z0NeP3RaLiqX6Q7Ml5CfEWbt-PysUbMNljuiLJw/get-owners");
         load("RASyFJyADTtG-l_Qe3a5PE_e2yUJR-PydXfkZjjrBuV7U/get-members");
         load("RAbq1a1FwRFAZPDde3Sy4GqNUQ2TmaKOWLydJPOyCKc0w/get-filtered-nanopub-list");
-        load("RAMvz3hcTY8LGe1pVVyGZg-Ga_FZgNYc9C1iuZPDuue6c/get-pinned-templates");
+        load("RAa2949qM3veXzp6rWoW-KNLSEbKOenMR9z3w9qQ48VsM/get-pinned-templates");
     }
 
     /**
--- src/main/java/com/knowledgepixels/nanodash/page/SpacePage.java
+++ src/main/java/com/knowledgepixels/nanodash/page/SpacePage.java
@@ -1,8 +1,6 @@
 package com.knowledgepixels.nanodash.page;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,9 +11,6 @@ import org.apache.wicket.extensions.ajax.markup.html.AjaxLazyLoadPanel;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.ExternalLink;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.eclipse.rdf4j.model.IRI;
 import org.nanopub.Nanopub;
@@ -26,9 +21,10 @@ import com.knowledgepixels.nanodash.User;
 import com.knowledgepixels.nanodash.Utils;
 import com.knowledgepixels.nanodash.component.ItemListElement;
 import com.knowledgepixels.nanodash.component.ItemListPanel;
-import com.knowledgepixels.nanodash.component.TemplateItem;
+import com.knowledgepixels.nanodash.component.PinGroupList;
 import com.knowledgepixels.nanodash.component.TitleBar;
-import com.knowledgepixels.nanodash.template.Template;
+
+
 
 /**
  * The ProjectPage class represents a space page in the Nanodash application.
@@ -75,13 +71,13 @@ public class SpacePage extends NanodashPage {
         add(new Label("description", "<span>" + Utils.sanitizeHtml(space.getDescription()) + "</span>").setEscapeModelStrings(false));
 
         if (space.isDataInitialized()) {
-            add(getPinPanel("pin-groups"));
+            add(new PinGroupList("pin-groups", space));
         } else {
             add(new AjaxLazyLoadPanel<Component>("pin-groups") {
     
                 @Override
                 public Component getLazyLoadComponent(String markupId) {
-                    return getPinPanel(markupId);
+                    return new PinGroupList(markupId, space);
                 }
     
                 @Override
@@ -123,45 +119,6 @@ public class SpacePage extends NanodashPage {
                 (space) -> new ItemListElement("item", SpacePage.class, new PageParameters().add("id", space), space.getLabel(), "(" + space.getTypeLabel() + ")")
             ));
 
-    }
-
-    private DataView<Pair<String, List<Serializable>>> getPinPanel(String markupId) {
-        final PageParameters params = new PageParameters();
-        if (space.getDefaultProvenance() != null) {
-            params.add("prtemplate", space.getDefaultProvenance().stringValue());
-        }
-
-        List<Pair<String, List<Serializable>>> pinnedResourcesList = new ArrayList<>();
-        List<String> pinGroupTags = new ArrayList<>(space.getPinGroupTags());
-        Collections.sort(pinGroupTags);
-        List<Serializable> pinnedResources = new ArrayList<>(space.getPinnedResources());
-        for (String tag : pinGroupTags) {
-            for (Object pinned : space.getPinnedResourceMap().get(tag)) {
-                if (pinnedResources.contains(pinned)) pinnedResources.remove(pinned);
-            }
-            pinnedResourcesList.add(Pair.of(tag, space.getPinnedResourceMap().get(tag)));
-        }
-        if (!pinnedResources.isEmpty()) {
-            String l = pinnedResourcesList.isEmpty() ? "Resources" : "Other Resources";
-            pinnedResourcesList.add(Pair.of(l, pinnedResources));
-        }
-
-        return new DataView<Pair<String, List<Serializable>>>(markupId, new ListDataProvider<>(pinnedResourcesList)) {
-
-            @Override
-            protected void populateItem(Item<Pair<String, List<Serializable>>> item) {
-                add(new ItemListPanel<Serializable>(
-                        "pinned-resource",
-                        item.getModelObject().getLeft(),
-                        item.getModelObject().getRight(),
-                        (o) -> {
-                            if (o instanceof Template t) return new TemplateItem("item", t, params);
-                            if (o instanceof IRI i) return new ItemListElement("item", ExplorePage.class, new PageParameters().add("id", i), i.stringValue());
-                            return null;
-                        }));
-            }
-
-        };
     }
 
     /**
