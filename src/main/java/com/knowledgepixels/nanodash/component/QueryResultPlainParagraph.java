3d55f4bb66a5514d2acad1c1eb610cb4d75814d7
Tobias Kuhn <kuhntobias@gmail.com>
2026-01-06 08:51:11 +0100

    Add source "^" link also to query result paragraphs

--- src/main/java/com/knowledgepixels/nanodash/component/QueryResultPlainParagraph.java
+++ src/main/java/com/knowledgepixels/nanodash/component/QueryResultPlainParagraph.java
@@ -2,12 +2,15 @@ package com.knowledgepixels.nanodash.component;
 
 import com.knowledgepixels.nanodash.QueryResult;
 import com.knowledgepixels.nanodash.ViewDisplay;
+import com.knowledgepixels.nanodash.page.ExplorePage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.nanopub.extra.services.ApiResponse;
 import org.nanopub.extra.services.ApiResponseEntry;
 import org.nanopub.extra.services.QueryRef;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
  * Component for displaying query results in a list format.
@@ -25,6 +28,20 @@ public class QueryResultPlainParagraph extends QueryResult {
     QueryResultPlainParagraph(String markupId, QueryRef queryRef, ApiResponse response, ViewDisplay viewDisplay) {
         super(markupId, queryRef, response, viewDisplay);
 
+        String label = grlcQuery.getLabel();
+        String paragraphTitle = response.getData().isEmpty() ? null : response.getData().get(0).get("title");
+        if (paragraphTitle != null && !paragraphTitle.isBlank()) {
+            label = paragraphTitle;
+        } else if (viewDisplay.getView() != null && viewDisplay.getView().getTitle() != null) {
+            label = viewDisplay.getView().getTitle();
+        }
+        add(new Label("label", label));
+        if (viewDisplay.getNanopubId() != null) {
+            add(new BookmarkablePageLink<Void>("np", ExplorePage.class, new PageParameters().set("id", viewDisplay.getNanopubId())));
+        } else {
+            add(new Label("np").setVisible(false));
+        }
+
         setOutputMarkupId(true);
         populateComponent();
     }
@@ -34,7 +51,6 @@ public class QueryResultPlainParagraph extends QueryResult {
         add(new ListView<>("paragraphs", response.getData()) {
             @Override
             protected void populateItem(ListItem<ApiResponseEntry> item) {
-                item.add(new Label("title", item.getModelObject().get("title")));
                 item.add(new Label("content", item.getModelObject().get("content")).setEscapeModelStrings(false));
             }
         });
