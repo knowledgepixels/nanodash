4c917e940cb57ca47c5ecaf29c349afde1429060
Ashley Caselli <ashley@knowledgepixels.com>
2026-02-27 16:27:46 +0100

    chore(domain): define ResourceWithProfile interface with essential methods

--- src/main/java/com/knowledgepixels/nanodash/domain/ResourceWithProfile.java
+++ src/main/java/com/knowledgepixels/nanodash/domain/ResourceWithProfile.java
@@ -1,5 +1,42 @@
 package com.knowledgepixels.nanodash.domain;
 
+import com.knowledgepixels.nanodash.Space;
+import com.knowledgepixels.nanodash.ViewDisplay;
+import org.eclipse.rdf4j.model.IRI;
+import org.nanopub.Nanopub;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Interface representing a resource that has an associated profile.
+ */
 public interface ResourceWithProfile {
+
+    String getId();
+
+    String getLabel();
+
+    String getNanopubId();
+
+    Nanopub getNanopub();
+
+    List<ViewDisplay> getViewDisplays();
+
+    List<ViewDisplay> getTopLevelViewDisplays();
+
+    List<ViewDisplay> getPartLevelViewDisplays(String resourceId, Set<IRI> classes);
+
+    Thread triggerDataUpdate();
+
+    void setDataNeedsUpdate();
+
+    boolean isDataInitialized();
+
+    Space getSpace();
+
+    List<AbstractResourceWithProfile> getAllSuperSpacesUntilRoot();
+
+    Long getRunUpdateAfter();
 
 }
