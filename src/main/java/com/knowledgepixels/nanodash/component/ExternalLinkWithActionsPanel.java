e836deda0780d05b2220568c8ef36154ce2428c8
Ashley Caselli <ashley@knowledgepixels.com>
2026-03-04 11:58:40 +0100

    feat(ExternalLinkWithActionsPanel): add panel for showing external links and allowing users to navigate/copy/explore them

---
+++ src/main/java/com/knowledgepixels/nanodash/component/ExternalLinkWithActionsPanel.java
@@ -0,0 +1,68 @@
+package com.knowledgepixels.nanodash.component;
+
+import com.knowledgepixels.nanodash.page.ExplorePage;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.ContextRelativeResourceReference;
+
+/**
+ * External link with Actions Panel that allows to copy the link and explore it through the Nanodash ExplorePage.
+ */
+public class ExternalLinkWithActionsPanel extends Panel {
+
+    private final IModel<String> urlModel;
+    private final IModel<String> labelModel;
+
+    public ExternalLinkWithActionsPanel(String id, IModel<String> urlModel, IModel<String> labelModel) {
+        super(id);
+        this.urlModel = urlModel;
+        this.labelModel = labelModel;
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        ExternalLink externalLink = new ExternalLink("externalLink", urlModel, labelModel);
+        add(externalLink);
+
+        AjaxLink<Void> copyLinkButton = new AjaxLink<>("copyLinkButton") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                String url = urlModel.getObject();
+                String escapedUrl = url.replace("'", "\\'");
+                target.appendJavaScript(
+                        "navigator.clipboard.writeText('" + escapedUrl + "')" +
+                        ".then(function() { alert('Link copied to clipboard!'); })" +
+                        ".catch(function(err) { console.error('Copy failed:', err); });"
+                );
+            }
+        };
+        copyLinkButton.add(new Image("copyIcon", new ContextRelativeResourceReference("images/copy-icon.svg", false)));
+        add(copyLinkButton);
+
+        AjaxLink<Void> exploreButton = new AjaxLink<>("exploreButton") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                setResponsePage(ExplorePage.class, new PageParameters().set("id", urlModel.getObject()));
+            }
+        };
+        exploreButton.add(new Label("exploreLabel", Model.of("Explore")));
+        add(exploreButton);
+    }
+
+    @Override
+    protected void onDetach() {
+        super.onDetach();
+        urlModel.detach();
+        labelModel.detach();
+    }
+
+}
